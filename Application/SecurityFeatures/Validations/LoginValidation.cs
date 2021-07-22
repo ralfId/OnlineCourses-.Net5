0f58529af54ebeb5a0c6687fbb0d185d993b0c5c
rafaelId <ralf_raid@outlook.es>
2021-07-22 12:26:26 -0600

    create login validations

---
+++ Application/SecurityFeatures/Validations/LoginValidation.cs
@@ -0,0 +1,15 @@
+﻿using Application.SecurityFeatures.Commands;
+using Domain.Models;
+using FluentValidation;
+
+namespace Application.SecurityFeatures.Validations
+{
+    public class LoginValidation : AbstractValidator<LoginCommand>
+    {
+        public LoginValidation()
+        {
+            RuleFor(u => u.Email).NotEmpty().EmailAddress();
+            RuleFor(u => u.Password).NotEmpty();
+        }
+    }
+}
