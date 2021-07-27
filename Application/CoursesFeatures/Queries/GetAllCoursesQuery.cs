d466d7c2907d652991d18caa3679953083c3c2f9
rafaelId <ralf_raid@outlook.es>
2021-07-27 09:42:06 -0600

    DTO's and automapper

--- Application/CoursesFeatures/Queries/GetAllCoursesQuery.cs
+++ Application/CoursesFeatures/Queries/GetAllCoursesQuery.cs
@@ -8,21 +8,39 @@ using Domain.Models;
 using System.Threading;
 using Persistence.Data;
 using Microsoft.EntityFrameworkCore;
+using Application.ModelsDto;
 
 namespace Application.CoursesFeatures.Queries
 {
-    public class GetAllCoursesQuery : IRequest<List<Courses>> { }
+    public class GetAllCoursesQuery : IRequest<List<CourseDto>> { }
 
-    public class GetAllCoursesQueryHandler : IRequestHandler<GetAllCoursesQuery, List<Courses>>
+    public class GetAllCoursesQueryHandler : IRequestHandler<GetAllCoursesQuery, List<CourseDto>>
     {
         private readonly OnlineCoursesContext _coursesContext;
-        public GetAllCoursesQueryHandler(OnlineCoursesContext coursesContext )
+        public GetAllCoursesQueryHandler(OnlineCoursesContext coursesContext)
         {
             _coursesContext = coursesContext;
         }
-        public async Task<List<Courses>> Handle(GetAllCoursesQuery request, CancellationToken cancellationToken)
+        public async Task<List<CourseDto>> Handle(GetAllCoursesQuery request, CancellationToken cancellationToken)
         {
-            return await _coursesContext.Courses.ToListAsync();
+            return await _coursesContext.Courses
+                .Include(x => x.courseInstructor)
+                .ThenInclude(x => x.Instructors)
+                .Select(x =>
+               
+                   new CourseDto()
+                   {
+                       CourseId = x.CourseId,
+                       Title = x.Title,
+                       Description = x.Description,
+                       PublicationDate = x.PublicationDate,
+                       CoverPhoto = x.CoverPhoto,
+                       Instructors = new InstructorDto()
+                       {
+                           InstructorId = x.courseInstructor.Id
+                       }
+                   }
+               ).ToListAsync();
         }
     }
 }
---
+++ Application/ModelsDto/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Domain.Models;
+
+namespace Application.ModelsDto
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Courses, CourseDto>();
+            CreateMap<Instructors, InstructorDto>();
+            CreateMap<CourseInstructor, CourseInstructorDto>();
+        }
+    }
+}
