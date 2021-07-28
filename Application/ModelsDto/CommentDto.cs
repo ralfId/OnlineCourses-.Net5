90d2664b8269fda3f4f78407c4bae0c893c737e6
rafaelId <ralf_raid@outlook.es>
2021-07-28 10:11:10 -0600

    Add CommentDto and PriceDto

---
+++ Application/ModelsDto/CommentDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Application.ModelsDto
+{
+    public class CommentDto
+    {
+        public Guid CommentId { get; set; }
+        public string Student { get; set; }
+        public int Score { get; set; }
+        public string CommentText { get; set; }
+        public Guid CourseId { get; set; }
+    }
+}
---
+++ Application/ModelsDto/PriceDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ModelsDto
+{
+    public  class PriceDto
+    {
+        public Guid PriceId { get; set; }
+        public decimal CurrentPrice { get; set; }
+        public decimal Promotion { get; set; }
+        public Guid CourseId { get; set; }
+    }
+}
