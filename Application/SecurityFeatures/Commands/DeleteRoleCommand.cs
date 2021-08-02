05026e7a5150c34416d16bd4d976c43669ddf499
rafaelId <ralf_raid@outlook.es>
2021-08-02 17:27:43 -0600

    delete role

---
+++ Application/SecurityFeatures/Commands/DeleteRoleCommand.cs
@@ -0,0 +1,48 @@
+﻿using Application.HandlersApplication;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.SecurityFeatures.Commands
+{
+    public class DeleteRoleCommand : IRequest
+    {
+        public string Name { get; set; }
+    }
+
+    public class DeleteRoleCommandHandler : IRequestHandler<DeleteRoleCommand>
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public DeleteRoleCommandHandler(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public async Task<Unit> Handle(DeleteRoleCommand request, CancellationToken cancellationToken)
+        {
+            var role = await _roleManager.FindByNameAsync(request.Name);
+
+            if (role == null)
+            {
+                throw new HandlerExceptions(HttpStatusCode.BadRequest, new { messaje = "Role not exist" });
+            }
+
+            var deleted = await _roleManager.DeleteAsync(role);
+
+            if (deleted.Succeeded)
+            {
+                return Unit.Value;
+            }
+            else
+            {
+                throw new HandlerExceptions(HttpStatusCode.InternalServerError, new { messaje = "Can't delete role" });
+            }
+            
+        }
+    }
+}
---
+++ Application/SecurityFeatures/Validations/DeleteRoleValidation.cs
@@ -0,0 +1,18 @@
+﻿using Application.SecurityFeatures.Commands;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.SecurityFeatures.Validations
+{
+    public class DeleteRoleValidation : AbstractValidator<DeleteRoleCommand>
+    {
+        public DeleteRoleValidation()
+        {
+            RuleFor(x => x.Name);
+        }
+    }
+}
